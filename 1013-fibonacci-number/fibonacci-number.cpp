5bf6241969adefac1525c8155e44387d60b11d14
Ashwin Pratap Singh <apsingh312007@gmail.com>
2026-07-03 21:51:27 +0530

    Time: 3 ms (65.44%) | Memory: 7.8 MB (37.54%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,10 +1,16 @@
 class Solution {
 public:
-    int recfeb(int n){
-        if(n==0 || n==1) return n;
-        return recfeb(n-1) + recfeb(n-2);
-    }
     int fib(int n) {
-        return recfeb(n);
+        if(n <= 1) return n;
+
+        int a = 0, b = 1;
+
+        for(int i = 2; i <= n; i++){
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+
+        return b;
     }
 };
