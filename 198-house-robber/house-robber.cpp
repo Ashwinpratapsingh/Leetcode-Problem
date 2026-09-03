bc792d6e20c5f3072f2beb447eb808ca790002fe
Ashwin Pratap Singh <apsingh312007@gmail.com>
2026-09-03 10:18:19 +0530

    Time: 0 ms (100.00%) | Memory: 10.8 MB (6.15%) - LeetSync

---
+++ 198-house-robber/house-robber.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int rob(vector<int>& nums) {
+        int n=nums.size();
+        vector<int>dp(n,-1);
+        return helper(0,nums,dp);
+    }
+    int helper(int i, vector<int>& nums,vector<int>& dp){
+        if(i>=nums.size()) return 0;
+        if(dp[i]!= -1) return dp[i];
+        int taken = nums[i]+helper(i+2,nums,dp);
+        int ntaken = helper(i+1,nums,dp);
+        dp[i] = max(taken,ntaken);
+        return dp[i];
+    }
+};
