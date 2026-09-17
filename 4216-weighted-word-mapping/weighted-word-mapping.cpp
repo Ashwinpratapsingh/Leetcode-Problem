72ca34aabdcb5392862285b620a4257b4467e3a7
Ashwin Pratap Singh <apsingh312007@gmail.com>
2026-09-17 13:31:12 +0530

    Time: 0 ms (100.00%) | Memory: 43.7 MB (55.35%) - LeetSync

---
+++ 4216-weighted-word-mapping/weighted-word-mapping.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string mapWordWeights(vector<string>& words, vector<int>& w) {
+        string s;
+        for(int i=0;i<words.size();i++){
+            int a=0;
+            for(int j=0;j<words[i].size();j++){
+                a+=w[words[i][j]-'a'];
+            }
+            a%=26;
+            s.push_back((char)(97+25-a));
+        }
+        return s;
+    }
+};
