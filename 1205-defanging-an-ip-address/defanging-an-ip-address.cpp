f7081761c702aa1bae891fdc6bde767de6f206d1
Ashwin Pratap Singh <apsingh312007@gmail.com>
2026-09-17 13:38:37 +0530

    Time: 3 ms (24.31%) | Memory: 7.8 MB (49.71%) - LeetSync

---
+++ 1205-defanging-an-ip-address/defanging-an-ip-address.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string defangIPaddr(string address) {
+        string ans;
+        for(char s:address){
+            if(s=='.') {
+                ans.push_back('[');
+                ans.push_back('.');
+                ans.push_back(']');
+            }
+            else ans.push_back(s);
+        }
+        return ans;
+        
+    }
+};
